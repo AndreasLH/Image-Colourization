b9d99b85edd631542cba9345651cf7d815aaed55
Andreas Lau Hansen <s194235@student.dtu.dk>
2021-11-15 14:43:46 +0100

    data almost works

--- eval.py
+++ eval.py
@@ -1,32 +1,30 @@
+from math import log10, sqrt
+import cv2
 import numpy as np
-  
-def PSNR(true, pred):
+
+def PSNR(original, compressed):
     '''
     Calculates the Peak signal to noise ratio between a ground truth image and predicted image.
 
     input: 
-        true image (PIL Image in RGB format)
-        predicted image (PIL Image in RGB format)
+        true image (cv2 image)
+        predicted image (cv2 image)
     output:
         PSNR score
     '''
-    true = np.array(true)
-    pred = np.array(pred)
-    mse = np.mean((true - pred) ** 2)
+    mse = np.mean((original - compressed) ** 2)
     if(mse == 0):  # MSE is zero means no noise is present in the signal .
                   # Therefore PSNR have no importance.
         return 100
     max_pixel = 255.0
-    psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+    psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
   
-  
 def main():
-    from PIL import Image
-    original = Image.open("test_imgs/original_image.png")
-    compressed = Image.open("test_imgs/compressed_image1.png")
-    value = PSNR(original, compressed)
-    print(f"PSNR value is {value} dB")
+     original = cv2.imread("test_imgs/original_image.png")
+     compressed = cv2.imread("test_imgs/compressed_image1.png", 1)
+     value = PSNR(original, compressed)
+     print(f"PSNR value is {value} dB")
        
 if __name__ == "__main__":
     main()
